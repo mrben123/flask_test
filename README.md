5c1af8971d87d3ebe32047f8753d940929e3223e
QD大侠 <mrbenlearnjs@gmail.com>
2022-10-09 10:22:22 +0800

    add some odds

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ flask run
 ## start a virtualments：
 python -m venv ./venv/
 
+## avtivate the virtualments:
+.\env\Scripts\activate
+
+## if the last step has err with the auth,please excute the next in role of root in windows powershell
+get-ExecutionPolicy
+&&
+set-ExecutionPolicy RemoteSigned
+
+
 ## pip list，show the env depends list
 
 ## install flask： 
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -8,7 +9,15 @@ def index():
 
 @app.route('/test')
 def test():
-    return "hello python test"
+    return "hello python test1"
+
+@app.route('/<name>')
+def name(name):
+    return f"hello python {escape(name)}"
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
